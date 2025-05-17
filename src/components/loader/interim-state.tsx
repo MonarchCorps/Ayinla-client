56d35010a4418f23f99e80aecf830dc9075326a0
MonarchCorps <monarchcorps01@gmail.com>
2025-05-17 02:32:49 +0100

    custom component for displaying loading, error or no data content

---
+++ src/components/loader/interim-state.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import clsx from "clsx";
+import { DotLottieReact } from '@lottiefiles/dotlottie-react';
+import React from "react";
+
+export const dynamic = "force-dynamic";
+
+export type LottieProps = {
+    animationData?: string,
+    autoplay?: boolean,
+    loop?: boolean,
+    className?: string;
+    text: React.ReactNode,
+    wrapperStyle?: string
+    textStyle?: string;
+    children?: React.ReactNode
+}
+
+export default function InterimState({
+    animationData,
+    autoplay = true,
+    loop = true,
+    className,
+    text,
+    wrapperStyle,
+    textStyle,
+    children
+}: LottieProps) {
+    return (
+        <div className={clsx("flex flex-col items-center text-center mt-10", wrapperStyle)}>
+            <div className="size-40">
+                <DotLottieReact
+                    src={animationData || "/lottie/loading.lottie"}
+                    loop={loop}
+                    autoplay={autoplay}
+                    className={clsx("size-40", className)}
+                />
+            </div>
+            <div className={clsx("font-semibold", textStyle)}>{text}</div>
+            {children}
+        </div>
+    );
+}
