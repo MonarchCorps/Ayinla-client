59d4fda3793fe72b0e6a6e77b14a707f1ac1c962
MonarchCorps <monarchcorps01@gmail.com>
2025-05-17 14:18:29 +0100

    updated ui

--- src/features/landing/tailored-properties/card.tsx
+++ src/features/landing/tailored-properties/card.tsx
@@ -1,36 +1,41 @@
+import { ListingType } from "@/types/Listing";
 import { FileImage, Proportions } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import { FaLocationDot } from "react-icons/fa6";
 
-export default function Card({ index }: { index: number }) {
+export default function Card({
+    listing
+}: {
+    listing: ListingType
+}) {
     return (
-        <Link href={`/listing/${index}`} prefetch className="shadow-md rounded-xl overflow-hidden">
-            <div className="relative w-full h-80">
+        <Link href={`/listing/${listing.slug}`} prefetch className="rounded-sm overflow-hidden">
+            <div className="relative w-full h-70 overflow-hidden">
                 <Image
-                    src={"/images/listing-image.webp"}
-                    alt="Listing Image" ////// don't forget to change the name
+                    src={listing.thumbnail_url}
+                    alt={listing.name}
                     className="object-cover transition-all duration-300 hover:scale-110"
                     fill
                 />
             </div>
-            <div className="pt-5 px-5 pb-3 space-y-3.5">
+            <div className="mt-3">
                 <p className="inline-flex items-center gap-x-1">
                     <FaLocationDot />
-                    <span className="text-sm font-medium">8765 Birchwood Street, CA</span>
+                    <span className="text-base font-medium">{listing.state} {listing.lga}</span>
                 </p>
-                <h1 className="text-xl font-semibold">Maple Grove</h1>
+                <h1 className="text-xl font-semibold mt-2">{listing.name}</h1>
                 <div className="flex items-center gap-x-2 mt-2">
                     <span className="flex items-center gap-x-1 opacity-55 text-sm border-r border-solid border-gray-800 pr-3">
                         <FileImage className="size-5" />
-                        <span>1</span>
+                        <span>{2}</span>
                     </span>
                     <span className="flex items-center gap-x-1 opacity-55 text-sm">
                         <Proportions className="size-5" />
                         <span>2,000 sq.ft</span>
                     </span>
                 </div>
-                <p className="font-medium text-base">₦{" "}{(400000).toLocaleString()}</p>
+                <p className="font-medium text-xl mt-3">₦{(listing.price_kobo / 100).toLocaleString()}</p>
             </div>
         </Link>
     );
